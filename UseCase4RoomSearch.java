c7c987e72886db9d0d09e0f019d93b16a76d90a0
Rithik R <rithikram.cr7@gmail.com>
2026-03-10 10:09:02 +0530

    Create UseCase4RoomSearch.java

---
+++ UseCase4RoomSearch.java
@@ -0,0 +1,16 @@
+public class UseCase4RoomSearch {
+
+public static void main(String[] args) {
+Room singleRoom = new SingleRoom();
+Room doubleRoom = new DoubleRoom();
+Room suiteRoom = new SuiteRoom();
+
+RoomInventory inventory = new RoomInventory();
+RoomSearchService searchService = new RoomSearchService();
+
+System.out.println("Room Search");
+System.out.println();
+
+searchService.searchAvailableRooms(inventory, singleRoom, doubleRoom, suiteRoom);
+}
+}
