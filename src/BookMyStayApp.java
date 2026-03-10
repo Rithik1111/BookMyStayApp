ea40df8ddb5200833762462de80d60cd090b3db3
Rithik R <rithikram.cr7@gmail.com>
2026-03-10 10:00:32 +0530

    Add initial hotel booking application structure

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,1 +1,7 @@
+public class UseCase1HotelBookingApp{
+public static void main(String[] args){
+System.out.println("welcome to book my stay");
+System.out.println("hotel booking system v1.0");
+}
+}
 
